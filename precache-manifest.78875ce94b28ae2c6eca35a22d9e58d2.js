885056dd95a37609d76bc782ca384853d990bb12
antoniakstepan <antoniakstepan1@gmail.com>
2021-03-09 12:29:54 +0200

    Updates

---
+++ precache-manifest.78875ce94b28ae2c6eca35a22d9e58d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c3f764d30e0511dad30b32943ccd3d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f99a74131ba67dd64bc5",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99a74131ba67dd64bc5",
+    "url": "./static/js/main.a7915c58.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
